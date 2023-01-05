9f027ccc0c1e982feb20cec19e9a499e0a3e0106
Matt Moore <illogical@users.noreply.github.com>
2023-01-04 20:03:20 -0500

    Add resource nodes and detect nearby resources

--- Assets/Scripts/ResourceGenerator.cs
+++ Assets/Scripts/ResourceGenerator.cs
@@ -4,14 +4,49 @@ using UnityEngine;
 
 public class ResourceGenerator : MonoBehaviour
 {
-    private BuildingTypeSO buildingType;
+    private ResourceGeneratorData resourceGeneratorData;
     float timer;
     float timerMax;
 
     public void Awake()
     {
-        buildingType = GetComponent<BuildingTypeHolder>().buildingType;
-        timerMax = buildingType.resourceGeneratorData.timerMax;
+        resourceGeneratorData = GetComponent<BuildingTypeHolder>().buildingType.resourceGeneratorData;
+        timerMax = resourceGeneratorData.timerMax;
+    }
+
+    private void Start()
+    {
+        Collider2D[] collisions = Physics2D.OverlapCircleAll(transform.position, resourceGeneratorData.resourceDetectionRadius);
+
+        int nearbyResourceAmount = 0;
+        foreach(Collider2D collision in collisions)
+        {
+            ResourceNode resourceNode = collision.GetComponent<ResourceNode>();
+            if (resourceNode != null)
+            {
+                if (resourceNode.resourceType == resourceGeneratorData.resourceType)
+                {
+                    // same type
+                    nearbyResourceAmount++;
+                }
+            }
+        }
+
+        nearbyResourceAmount = Mathf.Clamp(nearbyResourceAmount, 0, resourceGeneratorData.maxResourceAmount);
+
+        if(nearbyResourceAmount == 0)
+        {
+            // No resource nodes are nearby
+            // Disable resource generator
+            enabled = false;
+        }
+        else
+        {
+            // increase the amount of the timer as this is placed near more nodes. The smaller timerMax is, the faster resources are gathered.
+            timerMax = (resourceGeneratorData.timerMax / 2f) +
+                resourceGeneratorData.timerMax *
+                (1 - (float)nearbyResourceAmount / resourceGeneratorData.maxResourceAmount);
+        }
     }
 
     public void Update()
@@ -21,7 +56,14 @@ public class ResourceGenerator : MonoBehaviour
         if(timer <= 0f)
         {
             timer += timerMax;
-            ResourceManager.Instance.AddResource(buildingType.resourceGeneratorData.resourceType, 1);
+            ResourceManager.Instance.AddResource(resourceGeneratorData.resourceType, 1);
         }
+    }
+
+    public void OnDrawGizmos()
+    {
+        // display the distance a building needs to be from a resource to include it
+        Gizmos.DrawWireSphere(transform.position, resourceGeneratorData.resourceDetectionRadius);
+
     }
 }
--- Assets/Scripts/SpritePositionSortingOrder.cs
+++ Assets/Scripts/SpritePositionSortingOrder.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class SpritePositionSortingOrder : MonoBehaviour
 {
     [SerializeField] private bool runeOnce = true;
